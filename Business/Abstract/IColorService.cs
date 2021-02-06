65916219b568a5ab79c180f62e6a1091e91a1cbb
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-02-06 20:32:45 +0300

    düzenleme

---
+++ Business/Abstract/IColorService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IColorService
+    {
+        List<Color> GetAll();
+        void Add(Color color);
+        void Delete(Color color);
+        void Update(Color color);
+
+        Color GetById(int id);
+    }
+}
