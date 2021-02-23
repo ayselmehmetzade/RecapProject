d61d24e6848f8c0df4eac0fd7de7724ea49c32ef
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-02-24 00:27:16 +0300

    12. ders ödevi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,21 +22,14 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            //if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            //if (car.Description.Length<=2 && car.DailyPrice<0)
             //{
-            //    _carDal.Add(car);
-            //    Console.WriteLine("Ekleme işlemi başarılı!");
+            //    return new ErrorResult(Messages.CarInvalid);
             //}
-            //else
-            //{
-            //    Console.WriteLine("Lütfen tekrar deneyiniz. Açıklamanız 2 karakterden fazla ve günlük fiyat 0 dan büyük olmalıdır!");
-            //}
-            if (car.Description.Length<=2 && car.DailyPrice<0)
-            {
-                return new ErrorResult(Messages.CarInvalid);
-            }
+            //ValidationTool.Validate(new CarValidator(), car);
             _carDal.Add(car);
             return new SuccessResult(Messages.Added);
         }
@@ -40,7 +37,6 @@ namespace Business.Concrete
         public IResult Delete(Car car)
         {
             _carDal.Delete(car);
-            //Console.WriteLine("Silme işlemi başarılı!");
             return new SuccessResult(Messages.CarDeleted);
         }
 
@@ -51,54 +47,47 @@ namespace Business.Concrete
             //    return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
             //}
 
-            return new SuccessDataResult<List<Car>>( _carDal.GetAll(),Messages.Added);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.Added);
         }
 
         public IDataResult<List<CarDetailDto>> GetCarDetail()
         {
-            return new SuccessDataResult<List<CarDetailDto>>( _carDal.GetCarDetail());
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetail());
         }
 
         public IDataResult<List<Car>> GetCarsByBrandId(int id)
         {
-            return new SuccessDataResult<List<Car>>( _carDal.GetAll(c => c.BrandId == id));
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == id));
         }
 
         public IDataResult<List<Car>> GetCarsByColorId(int id)
         {
-            return new SuccessDataResult<List<Car>>( _carDal.GetAll(c => c.ColorId == id));
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == id));
         }
 
         public IDataResult<List<Car>> GetCarsByDailyPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Car>> (_carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max));
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max));
         }
 
         public IDataResult<List<Car>> GetCarsByDescription(string description)
         {
-            return new SuccessDataResult<List<Car>>( _carDal.GetAll(c => c.Description == description));
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.Description == description));
         }
 
         public IDataResult<Car> GetById(int id)
         {
-            return new SuccessDataResult<Car> (_carDal.Get(c => c.Id == id));
+            return new SuccessDataResult<Car>(_carDal.Get(c => c.Id == id));
         }
 
         public IResult Update(Car car)
         {
-            //if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            //if (car.Description.Length <= 2 && car.DailyPrice < 0)
             //{
-            //    _carDal.Update(car);
-            //    Console.WriteLine("Güncemele işlemi başarılı!");
+            //    return new ErrorResult(Messages.CarInvalid2);
             //}
-            //else
-            //{
-            //    Console.WriteLine("Lütfen tekrar deneyiniz. Açıklamanız 2 karakterden fazla ve günlük fiyat 0 dan büyük olmalıdır!");
-            //}
-            if (car.Description.Length <= 2 && car.DailyPrice < 0)
-            {
-                return new ErrorResult(Messages.CarInvalid2);
-            }
+
+            ValidationTool.Validate(new CarValidator(), car);
             _carDal.Update(car);
             return new SuccessResult(Messages.CarUpdated);
 
---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramewrok;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+           // SingleInstance=tek bir instance oluşturuyor. Datanın kendisini tutmaz. 
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+
+            builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+            builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+
+            builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
+            builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+
+            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+
+            builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
+            builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
+
+
+        }
+    }
+}
