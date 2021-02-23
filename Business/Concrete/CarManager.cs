e41ae3db445635ece9d8c3dc8c3df8c421f94350
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-02-24 01:05:59 +0300

    12.gün ödev düzenleme

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -22,6 +22,7 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+        
         [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramewrok;
 using System;
@@ -32,7 +35,13 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
             builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
 
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }
